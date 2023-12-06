2edbe97b8459c4951bebe691057769bb76f2b9c8
Dimitar <dpdimitrakov22@codingburgas.bg>
2023-12-06 13:04:56 +0200

    Add Login unverified home page Add footer in Register.html Add footer in forgot-password.html Crate JS folder Crate Logged.js Crate Logged.html

---
+++ JS/Logged.js
@@ -0,0 +1,11 @@
+//Log out button function
+function redirectToIndex() 
+{
+    window.location.href = "../index.html";
+}
+
+//Upload button function
+function enableUpload() 
+{
+    uploadButton.disabled = false;
+}
