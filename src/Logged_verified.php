4f13fc1435ce70c201edcd6fb3ec8f99b637630d
BRMilev22 <BRMilev22@codingburgas.bg>
2023-12-07 17:09:32 +0200

    rename css folder to styles; change css link paths

--- src/Logged_verified.php
+++ src/Logged_verified.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
-    <link rel="stylesheet" href="../css/Logged-Verified.css">
+    <link rel="stylesheet" href="../styles/Logged-Verified.css">
     <title>NGB</title>
 </head>
 <body>
