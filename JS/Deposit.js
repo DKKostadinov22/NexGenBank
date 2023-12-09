34a5428390f3b297916604a74ed8ea4343815bb2
BRMilev22 <BRMilev22@codingburgas.bg>
2023-12-09 13:31:00 +0200

    Fix deposit button #71

--- JS/Deposit.js
+++ JS/Deposit.js
@@ -3,19 +3,19 @@ document.getElementById('paymentForm').addEventListener('input', function()
 var cardNumber = document.getElementById('cardNumber').value;
 var expirationDate = document.getElementById('expirationDate').value;
 var cvv = document.getElementById('cvv').value;
-var submitButton = document.getElementById('submitButton');
+var submitButton = document.querySelector('.submitbtn');
 
-// Check if the card number is 16 digits
+//Check if the card number is 16 digits
 var isCardNumberValid = cardNumber.length === 16;
 
-// Check if the expiration date is in MM/YY format
+//Check if the expiration date is in MM/YY format
 var expirationDatePattern = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
 var isExpirationDateValid = expirationDate.match(expirationDatePattern);
 
-// Check if the CVV is 3 digits
+//Check if the CVV is 3 digits
 var isCVVValid = cvv.length === 3;
 
-// Enable/disable the submit button based on conditions
+//Enable/disable the submit button
 if (isCardNumberValid && isExpirationDateValid && isCVVValid) 
 {
     submitButton.removeAttribute('disabled');
