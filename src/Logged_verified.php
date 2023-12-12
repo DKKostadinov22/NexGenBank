00c11e249d63dc284df9dfc02f286eb70a73396d
Dimitar <dpdimitrakov22@codingburgas.bg>
2023-12-12 23:58:53 +0200

    Fix About Us Navbar Linking

--- src/Logged_verified.php
+++ src/Logged_verified.php
@@ -30,7 +30,7 @@
             <a class="nav-link active" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../pages/About-Us.html">About Us</a>
+            <a class="nav-link" href="../pages/About-Us-verified.html">About Us</a>
           </li>
           <li class="nav-item" id="nav-item-icon">
             <img src="../images/pngegg.png">
